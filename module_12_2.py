67d33584e6705f92833e48b27deb3c3b0852220c
V248V <demasiadaparami@gmail.com>
2024-11-06 13:12:20 +0300

    Add files via upload

--- module_12_2.py
+++ module_12_2.py
@@ -1,6 +1,7 @@
 import unittest
 import runner_and_tournament
 
+
 class TournamentTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -11,9 +12,6 @@ class TournamentTest(unittest.TestCase):
         self.andrey = runner_and_tournament.Runner('Андрей', speed=9)
         self.nick = runner_and_tournament.Runner('Ник', speed=3)
 
-
-
-
     @classmethod
     def tearDownClass(cls):
         for element in runner_and_tournament.Tournament.all_results.values():
@@ -22,14 +20,17 @@ class TournamentTest(unittest.TestCase):
     def test_tournament_1(self):
         tourn = runner_and_tournament.Tournament(90, self.usein, self.nick)
         runner_and_tournament.Tournament.all_results['test_1'] = tourn.start()
-        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)], 'Ник')
+        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)],
+                        'Ник')
 
     def test_tournament_2(self):
         tourn = runner_and_tournament.Tournament(90, self.andrey, self.nick)
         runner_and_tournament.Tournament.all_results['test_2'] = tourn.start()
-        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)], 'Ник')
+        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)],
+                        'Ник')
 
     def test_tournament_3(self):
         tourn = runner_and_tournament.Tournament(90, self.usein, self.andrey, self.nick)
         runner_and_tournament.Tournament.all_results['test_3'] = tourn.start()
-        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)], 'Ник')
+        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)],
+                        'Ник')
