7a512da8715709217bd1619f82e08d8836d546e2
V248V <demasiadaparami@gmail.com>
2024-11-06 13:11:57 +0300

    Add files via upload

---
+++ module_12_2.py
@@ -0,0 +1,35 @@
+import unittest
+import runner_and_tournament
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        runner_and_tournament.Tournament.all_results = {}
+
+    def setUp(self):
+        self.usein = runner_and_tournament.Runner('Усэйн', speed=10)
+        self.andrey = runner_and_tournament.Runner('Андрей', speed=9)
+        self.nick = runner_and_tournament.Runner('Ник', speed=3)
+
+
+
+
+    @classmethod
+    def tearDownClass(cls):
+        for element in runner_and_tournament.Tournament.all_results.values():
+            print(element)
+
+    def test_tournament_1(self):
+        tourn = runner_and_tournament.Tournament(90, self.usein, self.nick)
+        runner_and_tournament.Tournament.all_results['test_1'] = tourn.start()
+        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)], 'Ник')
+
+    def test_tournament_2(self):
+        tourn = runner_and_tournament.Tournament(90, self.andrey, self.nick)
+        runner_and_tournament.Tournament.all_results['test_2'] = tourn.start()
+        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)], 'Ник')
+
+    def test_tournament_3(self):
+        tourn = runner_and_tournament.Tournament(90, self.usein, self.andrey, self.nick)
+        runner_and_tournament.Tournament.all_results['test_3'] = tourn.start()
+        self.assertTrue(runner_and_tournament.Tournament.all_results[max(runner_and_tournament.Tournament.all_results)], 'Ник')
